f289be5891714a663956096d6e7de67e2f5ca5dd
Neeraj Yadav <neeraj0yadav0@gmail.com>
2022-06-18 18:27:44 +0530

    normal commit

--- src/Pages/Sidebar.js
+++ src/Pages/Sidebar.js
@@ -2,6 +2,18 @@ import React from "react";
 import styled from "styled-components";
 import FiberManualRecordIcon from "@material-ui/icons/FiberManualRecord";
 import CreateIcon from "@material-ui/icons/Create";
+import SidebarOption from "./SidebarOption";
+import CommentIcon from "@material-ui/icons/Comment";
+import InboxIcon from "@material-ui/icons/Inbox";
+import DraftsIcon from "@material-ui/icons/Drafts";
+import BookmarkBorderIcon from "@material-ui/icons/BookmarkBorder";
+import PeopleAltIcon from "@material-ui/icons/PeopleAlt";
+import AppsIcon from "@material-ui/icons/Apps";
+import FileCopyIcon from "@material-ui/icons/FileCopy";
+import ExpandLessIcon from "@material-ui/icons/ExpandLess";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import AddIcon from "@material-ui/icons/Add";
+
 function Sidebar() {
   return (
     <SidebarContainer>
@@ -15,6 +27,19 @@ function Sidebar() {
         </SidebarInfo>
         <CreateIcon />
       </SidebarHeader>
+
+      <SidebarOption Icon={CommentIcon} title="Threds" />
+      <SidebarOption Icon={InboxIcon} title="Mentions & reactions" />
+      <SidebarOption Icon={DraftsIcon} title="Saved items" />
+      <SidebarOption Icon={BookmarkBorderIcon} title="Channel Browser" />
+      <SidebarOption Icon={PeopleAltIcon} title="People & user groups" />
+      <SidebarOption Icon={AppsIcon} title="Apps" />
+      <SidebarOption Icon={FileCopyIcon} title="File Browser" />
+      <SidebarOption Icon={ExpandLessIcon} title="Show Less" />
+      <hr />
+      <SidebarOption Icon={ExpandMoreIcon} title="Channels" />
+      <hr />
+      <SidebarOption Icon={AddIcon} addChannelOption title="Add Channel" />
     </SidebarContainer>
   );
 }
@@ -28,6 +53,12 @@ const SidebarContainer = styled.div`
   border-top: 1px solid #49274b;
   max-width: 260px;
   margin-top: 60px;
+
+  > hr {
+    margin-top: 10px;
+    margin-bottom: 10px;
+    border: 1px solid #49274b;
+  }
 `;
 
 const SidebarHeader = styled.div`
@@ -54,8 +85,15 @@ const SidebarInfo = styled.div`
 
   > h3 {
     display: flex;
-    font-size: 13px;
+    font-size: 14px;
     font-weight: 400;
     align-items: center;
+  }
+
+  > h3 > .MuiSvgIcon-root {
+    font-size: 14px;
+    margin-top: 1px;
+    margin-right: 2px;
+    color: green;
   }
 `;
--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,7 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+
 const firebaseConfig = {
   apiKey: "AIzaSyACINWSPv-1yBaqrkJWOpF5l88hkm4rwjs",
   authDomain: "slack-clone-6789f.firebaseapp.com",
@@ -6,3 +10,12 @@ const firebaseConfig = {
   messagingSenderId: "813069969259",
   appId: "1:813069969259:web:d4bc3e50cab37922774e2e",
 };
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+
+export const auth = getAuth();
+
+export const provider = new GoogleAuthProvider();
+
+provider.setCustomParameters({ prompt: "select_account" });
